57ea0fd08b6920ce378638f4cae85001f303ee67
Chris Horne <chris@vocalogic.com>
2018-08-31 15:32:57 -0400

    moved labels

--- views/field-decorator.blade.php
+++ views/field-decorator.blade.php
@@ -1,20 +1,27 @@
-@if (in_array($type, ['raw', 'html', 'token', 'image', 'button', 'submit', 'reset']))
-  {!! $label !!}{!! $input !!}
-@else
-  <div class="form-group form-float">
-    <div class="form-line">
-      @if (!empty($pre))
-        <span class="input-group-addon">{!! $pre !!}</span>
-      @endif
-      {!! $input !!}
-      @if (!empty($post))
-        <span class="input-group-addon">{!! $post !!}</span>
-      @endif
-      {!! $label !!}
-    </div>
-    @if (!empty($comment))
-      <p class="small" style="color: #777; font-size:11px;">{{ $comment }}</p>
-    @endif
-  </div>
+@if(isset($card))
+    </div><div class="card-footer">
+@endif
 
+@if (in_array($type, ['raw', 'html', 'token', 'image', 'button', 'submit', 'reset']))
+    {!! $label !!}{!! $input !!}
+@else
+    <div class="form-group form-float">
+        <div class="form-line">
+            {!! $label !!}
+
+            @if (!empty($pre))
+                <span class="input-group-addon">{!! $pre !!}</span>
+            @endif
+            {!! $input !!}
+            @if (!empty($post))
+                <span class="input-group-addon">{!! $post !!}</span>
+            @endif
+        </div>
+        @if (!empty($comment))
+            <p class="small" style="color: #777; font-size:11px;">{{ $comment }}</p>
+        @endif
+    </div>
+@endif
+@if(isset($card))
+</div>
 @endif
